959b9e771d44b28aa74216cb146a59cec9a14142
Michael Washburn Jr <mdw7326@rit.edu>
2015-04-22 10:20:21 -0400

    Started using Volley, which is cool.
    The project now does a simple Http request to get the information at
    flashapi.mdwjr.com and sets the text on the Main Activity to reflect
    the response.

--- Flash-App/app/src/main/java/com/mdwjr/flash/MainActivity.java
+++ Flash-App/app/src/main/java/com/mdwjr/flash/MainActivity.java
@@ -1,39 +1,38 @@
 package com.mdwjr.flash;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends Activity {
 
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView text = (TextView)findViewById(R.id.response);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://flashapi.mdwjr.com";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        text.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                       text.setText("Error");
+                    }
+            });
+        queue.add(stringRequest);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
